d3b20560d59ecb503dfcb709ff24c35096a3e7cd
Stig Kleppe-Jørgensen <from.git@nisgits.net>
2011-12-16 10:54:33 +0100

    First cut on a sample

---
+++ src/sample/src/main/java/net/nisgits/gradle/android/sample/MyAndroidActivity.java
@@ -0,0 +1,15 @@
+package net.nisgits.gradle.android.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyAndroidActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
